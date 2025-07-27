6d1e38024d58df08e1c5cdf8b6a2ba9f82d016f7
mharis99 <mharis99@gmail.com>
2025-07-27 19:41:31 +0200

    use langchain

--- app/utils/gemini.server.ts
+++ app/utils/gemini.server.ts
@@ -1,22 +1,36 @@
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY!;
-const GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
+// const GEMINI_API_KEY = process.env.GEMINI_API_KEY!;
+// const GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
 
+
+// export async function getGeminiResponse(message: string): Promise<string> {
+
+//   const res = await fetch(`${GEMINI_API_URL}?key=${GEMINI_API_KEY}`, {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({
+//       contents: [{ parts: [{ text: message }] }]
+//     }),
+//   });
+
+//   const data = await res.json();
+//   console.log(`Gemini raw response (${res.status}): ${data}`);
+  
+//   return (
+//     data?.candidates?.[0]?.content?.parts?.[0]?.text ??
+//     "Sorry, I couldn't get anything"
+//   );
+// }
+
+
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+
+const model = new ChatGoogleGenerativeAI({
+  apiKey: process.env.GEMINI_API_KEY!,
+  modelName: "gemini-1.5-flash",
+});
 
 export async function getGeminiResponse(message: string): Promise<string> {
-
-  const res = await fetch(`${GEMINI_API_URL}?key=${GEMINI_API_KEY}`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      contents: [{ parts: [{ text: message }] }]
-    }),
-  });
-
-  const data = await res.json();
-  console.log(`Gemini raw response (${res.status}): ${data}`);
-  
-  return (
-    data?.candidates?.[0]?.content?.parts?.[0]?.text ??
-    "Sorry, I couldn't get anything"
-  );
+  const res = await model.invoke(message);
+  return res.content || "Sorry, no response.";
 }
+
