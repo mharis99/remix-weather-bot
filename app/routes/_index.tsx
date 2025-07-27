6d784cab1518759b5944103583c737477957bbbe
mharis99 <mharis99@gmail.com>
2025-07-27 20:18:01 +0200

    added dependency

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -45,7 +45,7 @@ export default function Index() {
         fontFamily: "sans-serif",
       }}
     >
-      <h1 style={{ textAlign: "center" }}>🌤️ AI Chat</h1>
+      <h1 style={{ textAlign: "center" }}>AI Chat</h1>
 
       <div
         style={{
