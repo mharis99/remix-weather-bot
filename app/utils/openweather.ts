7012a77aa2f5539932e4768f96e3884e19e4fafb
mharis99 <mharis99@gmail.com>
2025-07-27 21:47:09 +0200

    refactor

--- app/utils/openweather.ts
+++ app/utils/openweather.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getCurrentWeather(city: string): Promise<string> {
   const API_KEY = process.env.OPENWEATHER_API_KEY;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cd9e86837b6a5d50bf9b6209327b3294&units=metric`;
 
   const res = await axios.get(url);
   const data = res.data;
